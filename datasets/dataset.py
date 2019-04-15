1daf3369d002a435bf95c6a30e6ac448b7ef67de
Junkyung Kim <junkyungkim@Snowboard.local>
2019-04-15 19:58:01 -0400

    .

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -65,7 +65,6 @@ class Data(data.Dataset):
 		return self.transform(img, gt)
 
 	def transform(self, img, gt):
-		import ipdb;ipdb.set_trace()
 		gt = np.array(gt, dtype=np.float32)
 		if len(gt.shape) == 3:
 			gt = gt[:, :, 0]
@@ -140,6 +139,8 @@ class BSDS_data_jk(data.Dataset):
 		return self.transform(img, gt)
 
 	def transform(self, img, gt):
+		import ipdb;
+		ipdb.set_trace()
 		gt = np.array(gt, dtype=np.float32)
 		if len(gt.shape) == 3:
 			gt = gt[:, :, 0]
