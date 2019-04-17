b9b0386059b647a735e8346401dd457e8d90a805
Junkyung Kim <junkyungkim@Snowboard.local>
2019-04-16 21:34:01 -0400

    .

--- datasets/prebake_bsds_crops.py
+++ datasets/prebake_bsds_crops.py
@@ -28,7 +28,8 @@ for train_val in ['train','val']:
 
     # loop over examples and apply crops
     if not os.path.isdir(out_datset):
-        os.makedirs(out_datset)
+        os.makedirs(os.path.join(out_datset,'data', 'images'))
+        os.makedirs(os.path.join(out_datset, 'data', 'groundTruth'))
     for fn in files:
         print('filename: ' + fn)
         img = scipy.misc.imread(os.path.join(in_dataset, 'data', 'images', train_val, fn + '.jpg'))
