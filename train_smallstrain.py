52f4b6a3e68c9b2b17124b9b7331e1dffc2b872d
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-04-18 15:15:08 -0400

    .

--- train_smallstrain.py
+++ train_smallstrain.py
@@ -173,16 +173,6 @@ def train(model, args):
                 images, labels = images.cuda(), labels.cuda()
             images, labels = Variable(images), Variable(labels)
 
-            import ipdb;ipdb.set_trace()
-            batchid = 0
-            img_min = np.min(np.array(images.cpu()[batchid, :, :, :].flatten()))
-            img_max = np.max(np.array(images.cpu()[batchid, :, :, :].flatten()))
-            img_transposed = (np.transpose(np.array(images.cpu()[batchid, :, :, :]), (1, 2, 0)) - img_min)/(img_max-img_min)
-            gt_transposed = np.array(labels.cpu()[batchid,0,:,:])
-            plt.subplot(121);plt.imshow(img_transposed);
-            plt.subplot(122);plt.imshow(gt_transposed);
-            plt.show()
-
             out = model(images)
             # import ipdb;ipdb.set_trace()
             loss = 0
@@ -225,6 +215,19 @@ def train(model, args):
                     if args.cuda:
                         images, labels = images.cuda(), labels.cuda()
                     images, labels = Variable(images), Variable(labels)
+
+                    batchid = 0
+                    img_min = np.min(np.array(images.cpu()[batchid, :, :, :].flatten()))
+                    img_max = np.max(np.array(images.cpu()[batchid, :, :, :].flatten()))
+                    img_transposed = (np.transpose(np.array(images.cpu()[batchid, :, :, :]), (1, 2, 0)) - img_min) / (
+                    img_max - img_min)
+                    gt_transposed = np.array(labels.cpu()[batchid, 0, :, :])
+                    plt.subplot(121);
+                    plt.imshow(img_transposed);
+                    plt.subplot(122);
+                    plt.imshow(gt_transposed);
+                    plt.show()
+
                     out = model(images)
                     loss = 0
                     for k in xrange(10):
