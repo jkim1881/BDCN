e704f24d801c9e1fc7185d604859f9ce00896332
Junkyung Kim <junkyungkim@Snowboard.local>
2019-04-19 21:10:24 -0400

    .

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -395,7 +395,8 @@ class Tilt_illusion(data.Dataset):
 			self.metadata=self.metadata[:self.max_examples,:]
 
 		self.gt = self.metadata[:,4].astype(np.float)
-		self.gt_trig = np.concatenate((np.sin(np.pi * self.gt / 180.), np.cos(np.pi * self.gt / 180.)), axis=1)
+		self.gt_trig = np.concatenate(
+			np.expand_dims((np.sin(np.pi * self.gt / 180.), np.cos(np.pi * self.gt / 180.)),axis=1), axis=1)
 
 		self.__getitem__(10)
 
