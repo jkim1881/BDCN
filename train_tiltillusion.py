25e057033068417896040f5154dde0c5191eaf8c
Junkyung Kim <junkyungkim@Snowboard.local>
2019-04-21 00:41:07 -0400

    .

--- train_tiltillusion.py
+++ train_tiltillusion.py
@@ -24,9 +24,9 @@ def plot_grad_flow(named_parameters):
             if p.grad is not None:
                 layers.append(n)
                 ave_grads.append(p.grad.abs().mean())
-            else:
-                layers.append(n)
-                ave_grads.append(0)
+            # else:
+            #     layers.append(n)
+            #     ave_grads.append(0)
     plt.plot(ave_grads, alpha=0.3, color="b")
     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
