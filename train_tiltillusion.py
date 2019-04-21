edb152990b2c077e947888706ef45fd9254c85eb
Junkyung Kim <junkyungkim@Snowboard.local>
2019-04-21 00:08:58 -0400

    .

--- train_tiltillusion.py
+++ train_tiltillusion.py
@@ -20,9 +20,10 @@ def plot_grad_flow(named_parameters):
     ave_grads = []
     layers = []
     for n, p in named_parameters:
-        if(p.requires_grad) and ("bias" not in n):
-            layers.append(n)
-            ave_grads.append(p.grad.abs().mean())
+        if(p.requires_grad):
+            if p.grad is not None:
+                layers.append(n)
+                ave_grads.append(p.grad.abs().mean())
     plt.plot(ave_grads, alpha=0.3, color="b")
     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
