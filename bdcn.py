0f1f96a6fd1214feef9c788e61104f75f83ea8ba
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-05-22 22:29:22 -0400

    .

--- bdcn.py
+++ bdcn.py
@@ -191,7 +191,7 @@ class BDCN(nn.Module):
                 k = int(name.split('.')[0].split('_')[1])
                 param.copy_(get_upsampling_weight(1, 1, k*2))
             elif 'fuse' in name:
-                import ipdb;ipdb.set_trace()
+
                 num_params += param.size
                 if logger:
                     logger.info('init params %s ' % name)
@@ -200,6 +200,8 @@ class BDCN(nn.Module):
                 else:
                     nn.init.constant(param, 0.080)
             else:
+                import ipdb;
+                ipdb.set_trace()
                 num_params += param.size
                 if logger:
                     logger.info('init params %s ' % name)
