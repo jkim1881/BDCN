64184cb20e7c087f2827493cf4151b22dc5a1948
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-31 17:18:24 -0400

    .

--- plot_lc.py
+++ plot_lc.py
@@ -132,8 +132,16 @@ plt.scatter([0,1,2], [l for l in gnet_td],
             c='green', marker='o',
             s=70, alpha=0.6)
 plt.plot([0,1,2], [l for l in gnet_td],
-         c='black', marker='',
+         c='green', marker='',
          linestyle='-', linewidth=2, alpha=0.7)
+
+plt.scatter([0,1,2], [l for l in gnet_td],
+            c='green', marker='x',
+            s=70, alpha=0.6)
+plt.plot([0,1,2], [l for l in gnet_td],
+         c='green', marker='',
+         linestyle='-', linewidth=2, alpha=0.7)
+
 
 plt.xticks([0,1,2], ['1%','10%','100%'])
 plt.ylim(0.5,0.8)
