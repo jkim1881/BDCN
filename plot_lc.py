ea626c7fb43f9b25fcb9679115eb7e78f8c50cdc
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-31 17:17:14 -0400

    .

--- plot_lc.py
+++ plot_lc.py
@@ -135,10 +135,7 @@ plt.plot([0,1,2], [l for l in gnet_td],
          c='black', marker='',
          linestyle='-', linewidth=2, alpha=0.7)
 
-plt.plot([0,1,2], [0.803,0.803,0.803,0.803],
-         color='green', marker='',
-         linestyle='--', linewidth=2, alpha=0.6)
-plt.xticks([0,1,2,3], ['1%','10%','100%','A+100%'])
+plt.xticks([0,1,2], ['1%','10%','100%'])
 plt.ylim(0.5,0.8)
 plt.show()
 
