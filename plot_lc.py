211af0bf19965403a1fb8ba7d03181b7fc4857fc
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-31 11:39:20 -0400

    .

--- plot_lc.py
+++ plot_lc.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+## LC from data
 lc_npy_path = '/media/data_cifs/pytorch_projects/model_out_001data_01lr/learning_curves.npy'
 
 npy = np.load(lc_npy_path)
@@ -11,3 +12,19 @@ plt.figure(figsize=(3,3))
 plt.plot([pt[0] for pt in train_curve], [pt[1] for pt in train_curve])
 plt.plot([pt[0] for pt in val_curve], [pt[1] for pt in val_curve])
 plt.show()
+
+## Summary fig of training/val loss over conditions
+train_loss_1lr = [1771, 1312, 554]
+val_loss_1lr = [16335, 55464, 88482]
+plt.scatter([0,1,2], train_loss_1lr,
+            c='b', marker='o',
+            s=40, alpha=0.7)
+plt.plot([0,1,2], train_loss_1lr,
+         c='b', marker='',
+         linestyle='-', linewidth=1.4, alpha=0.7)
+plt.scatter([0,1,2], val_loss_1lr,
+            c='r', marker='o',
+            s=40, alpha=0.7)
+plt.plot([0,1,2], val_loss_1lr,
+         c='r', marker='',
+         linestyle='-', linewidth=1.4, alpha=0.7)
