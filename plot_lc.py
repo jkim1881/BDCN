f868941fb2e4d98e7c38a165981e99c5c9a71e50
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-31 10:58:41 -0400

    .

--- plot_lc.py
+++ plot_lc.py
@@ -5,7 +5,7 @@ lc_npy_path = '/media/data_cifs/pytorch_projects/model_out_001data_1lr/learning_
 
 npy = np.load(lc_npy_path)
 train_curve = npy[0]
-val_curve = npy[1]
+val_curve = [train_curve[0]] + npy[1]
 
 plt.plot([pt[0] for pt in train_curve],[np.log(pt[1]) for pt in train_curve])
 plt.plot([pt[0] for pt in val_curve],[np.log(pt[1]) for pt in val_curve])
