c452ca674870c420fdf6eeabfb152adc41441812
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-31 11:03:17 -0400

    .

--- plot_lc.py
+++ plot_lc.py
@@ -7,6 +7,6 @@ npy = np.load(lc_npy_path)
 train_curve = npy[0]
 val_curve = [train_curve[0]] + npy[1]
 
-plt.plot([pt[0] for pt in train_curve],[np.log(pt[1]) for pt in train_curve])
-plt.plot([pt[0] for pt in val_curve],[np.log(pt[1]) for pt in val_curve])
+plt.plot([pt[0] for pt in train_curve], [np.log10(pt[1]) for pt in train_curve])
+plt.plot([pt[0] for pt in val_curve], [np.log10(pt[1]) for pt in val_curve])
 plt.show()
